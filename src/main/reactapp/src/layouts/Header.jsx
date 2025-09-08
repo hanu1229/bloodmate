32a777ef140d19f3ff45c97618f9fc29731211be
hanu1229 <han494127@gmail.com>
2025-09-08 23:05:47 +0900

    2025-09-08 [리액트 관련 작성] 1. 리액트 자바 프로젝트에 적용 2. 메인페이지 구현 중

---
+++ src/main/reactapp/src/layouts/Header.jsx
@@ -0,0 +1,12 @@
+import "../styles/header.css";
+
+export default function Header(props) {
+    return <>
+        <div className = "header-main">
+            <ul>
+                <li>블러드메이트</li>
+                <li>로그인</li>
+            </ul>
+        </div>
+    </>;
+}
---
+++ src/main/reactapp/src/pages/App.jsx
@@ -0,0 +1,11 @@
+import Header from "../layouts/Header";
+
+export default function App(props) {
+    return <>
+        <Header></Header>
+        <div>
+            테스트입니당~
+        </div>
+    </>
+    
+}
